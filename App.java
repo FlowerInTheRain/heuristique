9046174f9add4204eaee33cd950ee9673873460c
Sid B <sid.bennaceur@haidi.io>
2024-06-29 15:03:53 +0200

    oopsi

--- App.java
+++ App.java
@@ -1,48 +1,21 @@
 public class App {
 
-    // calcule le prix payé par l'utilisateur dans le restaurant, en fonction de type de
-    // repas qu'il prend (assiette ou sandwich), de la taille de la boisson (petit, moyen, grand), du dessert et
-    // de son type (normal ou special) et si il prend un café ou pas (yes ou no).
-    // les prix sont fixes pour chaque type de chose mais des réductions peuvent s'appliquer
-    // si cela rentre dans une formule!
     public static int Compute(Menus menu, MenuType type, boolean coffee) {
-        // prix total à payer pour le client
         int total = 0;
         int coffeePrice = 1;
 
-        // le type ne peut être vide car le client doit déclarer au moins un repas
         if(menu == null || type == null) return -1;
         switch(menu){
-            case ASSIETTE_SMALL_BEVERAGE -> {
-                total = type.equals(MenuType.NORMAL) ? 17:19;
-            }
-            case ASSIETTE_MEDIUM_BEVERAGE -> {
-                total =  type.equals(MenuType.NORMAL)? 18:22;
-
-            }
-            case ASSIETTE_LARGE_BEVERAGE -> {
-                total =  21;
-            }
-            case SANDWICH_SMALL_BEVERAGE -> {
-                total =  type.equals(MenuType.NORMAL) ? 12:16;
-
-            }
-            case SANDWICH_MEDIUM_BEVERAGE -> {
-                total =  type.equals(MenuType.NORMAL) ? 13:17;
-
-            }
-            case SANDWICH_LARGE_BEVERAGE -> {
-                total =  type.equals(MenuType.NORMAL) ? 14:18;
-            }
+            case ASSIETTE_SMALL_BEVERAGE -> total = type.equals(MenuType.NORMAL) ? 17:19;
+            case ASSIETTE_MEDIUM_BEVERAGE -> total =  type.equals(MenuType.NORMAL)? 18:22;
+            case ASSIETTE_LARGE_BEVERAGE -> total =  21;
+            case SANDWICH_SMALL_BEVERAGE -> total =  type.equals(MenuType.NORMAL) ? 12:16;
+            case SANDWICH_MEDIUM_BEVERAGE -> total =  type.equals(MenuType.NORMAL) ? 13:17;
+            case SANDWICH_LARGE_BEVERAGE -> total =  type.equals(MenuType.NORMAL) ? 14:18;
         }
 
-        if (menu.equals(Menus.ASSIETTE_MEDIUM_BEVERAGE) && type.equals(MenuType.NORMAL) && coffee) {
-            System.out.print(" avec café offert!");
-        } else {
-            // Assume coffee costs 1 unit, adding to the total only if coffee is not included
-            if (!coffee) {
-                total += coffeePrice;
-            }
+        if (!menu.equals(Menus.ASSIETTE_MEDIUM_BEVERAGE) && !type.equals(MenuType.NORMAL) && coffee) {
+            total += coffeePrice;
         }
         return total;
     }
